32597c134e28e871cf59b0dc336bf7ba2f28bf9a
Nileshdeqode22 <nrajput@deqode.com>
2022-06-04 19:02:01 +0530

    added laxical,closures,passbyvalue and reference

---
+++ LexicalandClosures/lex.js
@@ -0,0 +1,53 @@
+/*What is Lexical Scope in JavaScript?
+Lexical scope describes how nested (also known as "child") functions have access to variables defined in parent scopes. 
+In JavaScript, people often confuse closures with lexical scope.
+
+Lexical scope is an important part of closures, but it is not a closure by itself.
+
+Closures are an advanced concept that is also a frequent topic of technical interviews.
+
+You should have a foundational understanding of functions before attempting to understand closures.
+*/
+
+const Myfunction=()=>{
+    let myValue=2;
+    console.log(myValue);
+
+    const childFunction=()=>{
+        console.log(myValue+1);
+    }
+    childFunction();
+
+
+}
+
+Myfunction()
+
+//What is a Closure in JavaScript?
+
+/*A closure is a function having access to the parent scope, even after the parent function has closed.
+Let's note the first part of the sentence before the comma:
+
+...a function having access to the parent scope
+That's describing lexical scope!
+
+But we need the second part of the definition to give an example of a closure...
+
+...even after the parent function has closed. */
+
+const myFunction = () => {
+    let myValue = 2;
+    console.log(myValue);
+
+    const childFunction = () => {
+         console.log(myValue += 1);
+    }
+
+    return childFunction;
+}
+
+const result = myFunction();
+console.log(result);
+result();
+result();
+result();
---
+++ passbyvaluepassbyrefernce/pv.js
@@ -0,0 +1,19 @@
+function PassByValue(a,b){
+    let temp;
+    tmp=b;
+    b=a;
+    a=tmp;
+    console.log(`Inside Pass by value 
+        function -> a = ${a} b = ${b}`);
+
+}
+
+let a = 1;
+let b = 2;
+console.log(`Before calling Pass by value 
+        Function -> a = ${a} b = ${b}`);
+  
+PassByValue(a, b);
+  
+console.log(`After calling Pass by value 
+       Function -> a =${a} b = ${b}`);
